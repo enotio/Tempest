19e41ac51ecef5b274827ba824633bb5afb9ac09
Try <try9998@gmail.com>
2014-01-07 22:51:56 +0400

    array semantic for utility classes

--- Tempest/utils/utility.h
+++ Tempest/utils/utility.h
@@ -9,7 +9,14 @@ namespace Tempest{
   struct Point{
     Point():x(0), y(0) {}
     explicit Point( int ix, int iy=0 ):x(ix), y(iy) {}
-    int x, y;
+
+    union{
+      struct {
+        int x, y;
+        };
+
+      int data[2];
+    };
 
     Point& operator -= ( const Point & p );
     Point& operator += ( const Point & p );
@@ -45,7 +52,15 @@ namespace Tempest{
   struct Size{
     Size( int is = 0 ):w(is), h(is) {}
     Size( int ix, int iy ):w(ix), h(iy) {}
-    int w, h;
+
+    union{
+      struct {
+        int w, h;
+        };
+
+      int data[2];
+    };
+
 
     Point toPoint() const;
     Rect  toRect() const;
@@ -63,7 +78,15 @@ namespace Tempest{
           int iw,
           int ih )
       :x(ix), y(iy), w(iw), h(ih) {}
-    int x,y,w,h;
+    //int x,y,w,h;
+    union{
+      struct {
+        int x, y, w, h;
+        };
+
+      int data[4];
+    };
+
 
     Point pos() const;
     Size  size() const;
